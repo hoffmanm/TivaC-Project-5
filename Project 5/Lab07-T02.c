fb4df0718ca3c24afdb0763f205bdd3db6c900bf
hoffmanm <hoffmanm@unlv.nevada.edu>
2018-07-02 13:35:54 -0700

    Add files via upload

---
+++ Project 5/Lab07-T02.c
@@ -0,0 +1,102 @@
+#include <stdint.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+#include "inc/hw_memmap.h"
+#include "inc/hw_types.h"
+#include "driverlib/gpio.h"
+#include "driverlib/debug.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/adc.h"
+#include "driverlib/rom.h"
+#include "driverlib/pin_map.h"
+#include "driverlib/uart.h"
+
+
+void print_string(char* printMe)
+{
+    while (*printMe != 0x00)
+    {
+        UARTCharPut(UART0_BASE,*printMe);
+        printMe++;
+    }
+}
+
+
+char * reverse(char *s)
+{
+    uint8_t i, j;
+    char c;
+    j = strlen(s)-1;
+    for (i = 0; i<j; i++, j--) {
+        c = s[i];
+        s[i] = s[j];
+        s[j] = c;
+    }
+    return s;
+}
+
+void print_integer(int n)
+{
+    char s[5];
+    uint8_t i;
+    i = 0;
+    do {       /* generate digits in reverse order */
+        s[i++] = n % 10 + '0';   /* get next digit */
+    } while ((n /= 10) > 0);     /* delete it */
+    s[i] = '\0';
+    print_string(reverse(s));
+}
+
+
+int main(void)
+{
+    uint32_t ui32ADC0Value[4];
+    volatile uint32_t ui32TempAvg;
+    volatile uint32_t ui32TempValueC;
+    volatile uint32_t ui32TempValueF;
+
+    ROM_SysCtlClockSet(SYSCTL_SYSDIV_5|SYSCTL_USE_PLL|SYSCTL_OSC_MAIN|SYSCTL_XTAL_16MHZ);
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_UART0);
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
+
+    GPIOPinConfigure(GPIO_PA0_U0RX);
+    GPIOPinConfigure(GPIO_PA1_U0TX);
+    GPIOPinTypeUART(GPIO_PORTA_BASE, GPIO_PIN_0 | GPIO_PIN_1);
+
+    UARTConfigSetExpClk(UART0_BASE, SysCtlClockGet(), 115200,
+        (UART_CONFIG_WLEN_8 | UART_CONFIG_STOP_ONE | UART_CONFIG_PAR_NONE));
+
+
+    ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_ADC0);
+    ROM_ADCHardwareOversampleConfigure(ADC0_BASE, 64);
+
+    ROM_ADCSequenceConfigure(ADC0_BASE, 1, ADC_TRIGGER_PROCESSOR, 0);
+    ROM_ADCSequenceStepConfigure(ADC0_BASE, 1, 0, ADC_CTL_TS);
+    ROM_ADCSequenceStepConfigure(ADC0_BASE, 1, 1, ADC_CTL_TS);
+    ROM_ADCSequenceStepConfigure(ADC0_BASE, 1, 2, ADC_CTL_TS);
+    ROM_ADCSequenceStepConfigure(ADC0_BASE,1,3,ADC_CTL_TS|ADC_CTL_IE|ADC_CTL_END);
+    ROM_ADCSequenceEnable(ADC0_BASE, 1);
+
+    while(1)
+    {
+        ROM_ADCIntClear(ADC0_BASE, 1);
+        ROM_ADCProcessorTrigger(ADC0_BASE, 1);
+
+       while(!ROM_ADCIntStatus(ADC0_BASE, 1, false))
+       {
+       }
+
+       ROM_ADCSequenceDataGet(ADC0_BASE, 1, ui32ADC0Value);
+       ui32TempAvg = (ui32ADC0Value[0] + ui32ADC0Value[1] + ui32ADC0Value[2] + ui32ADC0Value[3] + 2)/4;
+       ui32TempValueC = (1475 - ((2475 * ui32TempAvg)) / 4096)/10;
+       ui32TempValueF = ((ui32TempValueC * 9) + 160) / 5;
+       print_string("\n\rAverage:");
+       print_integer(ui32TempAvg);
+       print_string("\n\rCelsius:");
+       print_integer(ui32TempValueC);
+       print_string("\n\rFahrenheit:");
+       print_integer(ui32TempValueF);
+       SysCtlDelay(1000000);
+    }
+}
